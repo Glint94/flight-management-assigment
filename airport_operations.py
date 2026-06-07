7090c02e81ccc574a4c8b1bce34bf4c9d09fafe6
Taylor Mashford <taylormashford94@gmail.com>
2026-06-07 22:04:49 +0000

    Implemented airport operations

--- airport_operations.py
+++ airport_operations.py
@@ -21,25 +21,24 @@ def show_available_airports():
     database = connect_to_database()
     cursor = database.cursor()
 
-    cursor.execute('''
-                    SELECT airport_code, airport_name
-                    FROM airports
-                    ORDER BY airport_code
-    ''')
+    cursor.execute('''SELECT airport_code, airport_name FROM airports ORDER BY airport_code''')
 
-    print("\nAvailable Airports")
+    print("\n--Available Airports--")
 
     for airport in cursor.fetchall():
+        print(f"{airport[0]} - {airport[1]}")   
 
-        print(
-            f"{airport[0]} - {airport[1]}"
-        )   
+    database.close()
 
 def add_new_airport():
     database = None
 
     try:
         airport_code = input("Airport Code: ").upper()
+
+        if len(airport_code) != 3:
+            print("Airport Code must be exactly 3 characters long.")
+            return
 
         airport_name = input("Airport Name: ")
 
@@ -70,8 +69,8 @@ def add_new_airport():
 
         print("Airport added successfully.")
 
-    except sqlite3.IntegrityError:
-        print("Airport code already exists.")
+    except sqlite3.IntegrityError as error:
+        print(f"Database error: {error}")
 
     finally:
         if database is not None:
@@ -81,7 +80,7 @@ def view_all_airports():
     database = connect_to_database()
     cursor = database.cursor()
 
-    cursor.execute('''SELECT * FROM airports ORDER BY airport_code''')
+    cursor.execute('''SELECT airport_code, airport_name, city, country FROM airports ORDER BY airport_code''')
 
     airports = cursor.fetchall()
 
@@ -91,9 +90,115 @@ def view_all_airports():
 
     database.close()
 
+def update_airport_name():
+    database = None
+
+    try:
+        show_available_airports()
+
+        airport_code = input("Airport Code: ").upper()
+        airport_id = get_airport_id(airport_code)
+
+        if airport_id is None:
+            print("Invalid Airport Code")
+            return
+
+        airport_name = input("New Airport Name: ")
+
+        database = connect_to_database()
+        cursor = database.cursor()
+
+        cursor.execute('''UPDATE airports SET airport_name = ? WHERE airport_id = ?''', (airport_name, airport_id))
+
+        database.commit()
+
+        print("Airport Name updated.")
+
+    finally:
+        if database is not None:
+            database.close()
+
+def update_city():
+    database = None
+
+    try:
+        show_available_airports()
+
+        airport_code = input("Airport Code: ").upper()
+        airport_id = get_airport_id(airport_code)
+
+        if airport_id is None:
+            print("Invalid Airport Code")
+            return
+
+        city = input("New Airport City: ")
+
+        database = connect_to_database()
+        cursor = database.cursor()
+
+        cursor.execute('''UPDATE airports SET city = ? WHERE airport_id = ?''', (city, airport_id))
+
+        database.commit()
+
+        print("City updated.")
+
+    finally:
+        if database is not None:
+            database.close()
+
+def update_country():
+    database = None
+
+    try:
+        show_available_airports()
+
+        airport_code = input("Airport Code: ").upper()
+        airport_id = get_airport_id(airport_code)
+
+        if airport_id is None:
+            print("Invalid Airport Code")
+            return
+
+        country = input("New Airport Country: ")
+
+        database = connect_to_database()
+        cursor = database.cursor()
+
+        cursor.execute('''UPDATE airports SET country = ? WHERE airport_id = ?''', (country, airport_id))
+
+        database.commit()
+
+        print("Country updated.")
+
+    finally:
+        if database is not None:
+            database.close()
 
 def update_airport_menu():
-    print("Update Airports")
+    
+    while True:
+
+        print("\n--Update Airport Menu--")
+        print("1. Update Airport Name")
+        print("2. Update City")
+        print("3. Update Country")
+        print("4. Return")
+
+        choice = input("Select option: ")
+        if choice == "1":
+            update_airport_name()
+
+        elif choice == "2":
+            update_city()
+
+        elif choice == "3":
+            update_country()
+
+        elif choice == "4":
+            break
+
+        else:
+            print("Invalid option, try again.")
 
 def airport_functions():
 
